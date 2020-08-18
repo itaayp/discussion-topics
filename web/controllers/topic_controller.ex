2c8b8f5fef0fb94974b8debcecf5d3461657794a
Itay Peceniski <itay.peck@gmail.com>
2020-08-18 20:04:07 -0300

    class_78 - created the logic to insert a topic to the database; created the error message displayed on the HTML when the insertion was not completed

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,14 +1,30 @@
 defmodule Discuss.TopicController do
+    @moduledoc """
+    This is the Topic Controller and will orchestrate the actions related to the Topic
+    """
     use Discuss.Web, :controller
     alias Discuss.Topic
-
+    
+    @doc """
+    This function renderizes the New Topic screen.
+    The changeset is needed to be passed by parameter when calling the `render` function
+    """
     def new(conn, _params) do
         changeset = Topic.changeset(%Topic{}, %{})
 
         render conn, "new.html", changeset: changeset
     end
 
+    @doc """
+    This function adds a new row in the `topics` table on the database 
+    """
     def create(conn, %{"topic" => topic}) do
-        
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repos.insert(changeset) do
+            {:ok, post} -> IO.inspect(post)
+            {:error, changeset} -> 
+                render conn, "new.html", changeset: changeset
+        end
     end
 end
