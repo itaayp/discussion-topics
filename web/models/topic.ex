7589000f1f88c8a6ae0374befd6c8e3f689fe33f
Itay Peceniski <itay.peck@gmail.com>
2020-08-19 09:12:14 -0300

    created documentation to the Topic Model

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -1,10 +1,22 @@
 defmodule Discuss.Topic do
+@moduledoc """
+This is the Topic's Model from MVC.
+This module is responsible to handle the data
+"""
     use Discuss.Web, :model
 
+    @doc """
+    This is the creation of a table `topics` with a column called `title` on the database
+    """
     schema "topics" do
         field :title, :string
     end
 
+    @doc """
+    This function cast a information from the database to a Elixir format and validates the information
+    The params of the function is a `struct` wich will be used to identify the table on the database, and a `params`, that if the `params` is `nil` or it's not passed, the default value is `%{}`
+    """
+    # params \\ %{} defines a default value for params
     def changeset(struct, params \\ %{}) do
         struct
         |> cast(params, [:title])
